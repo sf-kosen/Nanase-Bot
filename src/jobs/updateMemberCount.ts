1fd5e4f37746e5023804b33962ce59e815240cba
sora81dev <souta@prv.sora81.dev>
2025-12-28 08:56:33 +0900

    表記を変更 (#6)

--- src/jobs/updateMemberCount.ts
+++ src/jobs/updateMemberCount.ts
@@ -7,23 +7,26 @@ export function startMemberCountJob(client: Client): NodeJS.Timeout {
 
   console.log("Starting member count job...");
 
-  return setInterval(async () => {
-    try {
-      console.log("Updating member count...");
+  return setInterval(
+    async () => {
+      try {
+        console.log("Updating member count...");
 
-      const guild = client.guilds.cache.get(GUILD_ID);
-      if (!guild) return;
+        const guild = client.guilds.cache.get(GUILD_ID);
+        if (!guild) return;
 
-      const channel = guild.channels.cache.get(CHANNEL_ID);
-      if (!channel || !channel.isVoiceBased()) return;
+        const channel = guild.channels.cache.get(CHANNEL_ID);
+        if (!channel || !channel.isVoiceBased()) return;
 
-      const { humans: memberCount } = await getMemberStatus(guild);
+        const { humans: memberCount } = await getMemberStatus(guild);
 
-      await channel.setName(`Member: ${memberCount}`);
-      console.log(`Updated member count in ${channel.name}`);
-    } catch (error) {
-      console.error(`Error updating member count: ${error}`);
-    }
-    /* 2時間ごとに繰り返す */
-  }, 2 * 60 * 60 * 1000);
+        await channel.setName(`学生: ${memberCount}人`);
+        console.log(`Updated member count in ${channel.name}`);
+      } catch (error) {
+        console.error(`Error updating member count: ${error}`);
+      }
+      /* 2時間ごとに繰り返す */
+    },
+    2 * 60 * 60 * 1000,
+  );
 }
