3e16449dfe2a7fbfddf81db412da6c5c71b7d737
MikanNigata <yukichiba2101@gmail.com>
2026-04-17 11:47:25 +0900

    修正: リアクションロール対象メッセージの検出不具合を直す

---
+++ src/jobs/checkReactionRoleMessage.ts
@@ -0,0 +1,43 @@
+import { Client } from "discord.js";
+
+const reactionRoleChannelID = process.env["REACTIONROLE_CHANNEL_ID"]!;
+const botID = process.env["BOT_ID"]!;
+
+const REACTION_ROLE_MESSAGE = [
+  "リアクションしてロールを付与しよう！",
+  ":bell: : 通知勢 : たくさん通知が届くよ！",
+  ":sound: : VC募集 : VCに参加したい人向け！",
+].join("\n");
+
+export default async function checkReactionRoleMessage(
+  client: Client,
+): Promise<string | null> {
+  const channel = await client.channels.fetch(reactionRoleChannelID);
+
+  if (channel && channel.isTextBased()) {
+    if (channel.partial) {
+      await channel.fetch();
+    }
+
+    if (!("send" in channel)) {
+      throw new Error("This channel can't send msg");
+    }
+
+    const messages = await channel.messages.fetch({ limit: 10 });
+
+    const targetMessage = messages.find(
+      (m) =>
+        m.author.id === botID &&
+        m.content.includes("リアクションしてロールを付与しよう！"),
+    );
+
+    if (targetMessage) {
+      return targetMessage.id;
+    }
+
+    const sentMessage = await channel.send(REACTION_ROLE_MESSAGE);
+    return sentMessage.id;
+  }
+
+  throw new Error("This channel can't send msg");
+}
