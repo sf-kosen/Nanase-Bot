4683c1450d958b05a8298c06096a67400da3bfc8
sora81dev <souta@prv.sora81.dev>
2026-07-05 20:44:00 +0900

    refactor: migrate VC_ROLE_ID and NOTIFIER_ROLE_ID to configFile

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -6,4 +6,8 @@ export const env = {
   tokens: {
     discordToken: process.env.DISCORD_TOKEN,
   },
+  role_id: {
+    notifier: process.env.NOTIFIER_ROLE_ID,
+    vc: process.env.VC_ROLE_ID,
+  },
 };
