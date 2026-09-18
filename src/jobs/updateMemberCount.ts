b747190a97b76d0325686400afa1106c9da6628c
Sora <117363029+sora81dev@users.noreply.github.com>
2026-09-18 20:28:58 +0900

    fix: make constant

--- src/jobs/updateMemberCount.ts
+++ src/jobs/updateMemberCount.ts
@@ -1,7 +1,7 @@
 import type { Client } from "discord.js";
 
-const CHANNEL_ID = "1454473598973509697";
 const GUILD_ID = "1452263053180534806";
+const MEMBER_ROLE_ID = "1454446371221536788";
 
 export async function updateMemberCount(client: Client) {
   console.log("[INFO]  Starting member count job...");
