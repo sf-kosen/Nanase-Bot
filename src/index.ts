e315f75d3116e97ab91398b4d8990a04089bf0a1
sora81dev <117363029+sora81dev@users.noreply.github.com>
2026-09-26 23:26:35 +0900

    chore (joinRole): remove unused varriables

--- src/index.ts
+++ src/index.ts
@@ -220,9 +220,6 @@ client.on("voiceStateUpdate", handleVcJoin);
 client.on("voiceStateUpdate", handleVcLeave);
 
 client.on("guildMemberAdd", async (member) => {
-  const time = Date.now();
-  const date = new Date(time);
-
   if (member.user.bot) {
     // BOTロールを付与
     await addRoleSafely(member, "1454099602641780737", "bot");
